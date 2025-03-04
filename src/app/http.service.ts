a3f5bd2bb04f98c72fb5de2df9e41578c3876df0
Sudarshan-W <waghmaresudarshan2002@gmail.com>
2025-03-04 23:26:57 +0530

    Add files via upload
    Frontend files

---
+++ src/app/http.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  baseUrl:string='http://localhost:8080/api/'
+  constructor(private http:HttpClient) { }
+  
+  registerData(obj:any){
+    return (this.http.post(`${this.baseUrl}user/save`,obj,{
+      responseType:"text"
+    }));
+  }
+  login(obj:any){
+    return (this.http.post(`${this.baseUrl}user/login`,obj))
+
+  }
+  getAllEmpRecors(){
+   return (this.http.get(`${this.baseUrl}emp/findall`))
+  }
+  getParticularRecordById(id:any){
+    return (this.http.get(`${this.baseUrl}emp/findbyid/${id}`))
+  }
+  getAllCountry(){
+   return (this.http.get(`${this.baseUrl}country/findall`))
+  }
+  postEmpData(obj:any){
+    return (this.http.post(`${this.baseUrl}emp/save`,obj,{
+      responseType:"text" 
+      }));
+  }
+  updateEmpData(obj:any){
+    return (this.http.put(`${this.baseUrl}emp/update`,obj,{
+      responseType:"text"
+    }));
+  }
+  deleteEmpData(id:any){
+    return (this.http.delete(`${this.baseUrl}emp/deletebyid/${id}`,{
+      responseType:"text"
+    }));
+  }
+}  
